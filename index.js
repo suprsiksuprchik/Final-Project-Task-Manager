d1639c6a1d4b76187379172a5de81f19973a3259
Darlene Bromund <101606559+suprsiksuprchik@users.noreply.github.com>
2022-05-04 22:26:54 +0000

    add console.log(tasks)

---
+++ index.js
@@ -0,0 +1,21 @@
+const taskManager = new TaskManager(0);
+task.addTask();
+console.log(tasks);
+
+
+/* const newTaskForm = document.querySelector('#newTaskForm');
+
+newTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const newTaskDescription = document.querySelector('#newTaskDescription');
+    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+    const newTaskDueDate = document.querySelector('#newTaskDueDate');
+    const errorMessage = document.querySelector('#alertMessage');
+
+    const name = newTaskNameInput.value;
+    const description = newTaskDescription.value;
+    const assignedTo = newTaskAssignedTo.value;
+    const dueDate = newTaskDueDate.value;
+  */
--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,7 +1,22 @@
 
-class TaskManager{
+class TaskManager {
   constructor(currentId = 0){
-    this.task = [];
+    this.tasks = [];
     this.currentId = currentId;
-  }  
+  } 
+
+  addTask(name, description, assignedTo, dueDate) {
+     const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,  
+      status: 'TODO'
+    };
+  
+    return this.tasks.push(task);
+  }
 }
+
+
