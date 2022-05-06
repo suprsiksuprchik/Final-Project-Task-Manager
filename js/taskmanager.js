e9a03637f9db57730f436a0fb81cea0c1bcf69c9
Darlene Bromund <101606559+suprsiksuprchik@users.noreply.github.com>
2022-05-06 16:55:31 +0000

    add an 's' to line 42 this.task's'

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,17 +1,18 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => `
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => `
   <li class="list-group-item" data-task-id=${id}>
         <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
             <h5>${name}</h5>
-            <span class="badge badge-danger">${status}                </span>
+            <span class="badge badge-danger">${status}</span>
             </div>
           <div class="d-flex w-100 mt-2 justify-content-between">
                 <small>${assignedTo}</small>
                 <small>${dueDate}</small>
           </div>
           <p>${description}</p>
-          <button class="done-button">
-            Mark As Done
-          </button>
+          <div class="d-flex w-100 justify-content-end">
+            <button class="done-button">
+              Mark As Done
+            </button>
   </li>`;
                                                           
 
@@ -38,7 +39,7 @@ class TaskManager {
       let foundTask;
   
       for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.task[i];
+        const task = this.tasks[i];
   
         if (task.id === taskId) {
           foundTask = task;
@@ -55,17 +56,18 @@ class TaskManager {
       const task = this.tasks[i];
 
       const date = new Date(task.dueDate);
-      const formattedDate = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+      const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
 
       const taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
 
       tasksHtmlList.push(taskHtml);
     }
+    
     const tasksHtml = tasksHtmlList.join('\n');
 
     const tasksList = document.querySelector('#tasksList');
     tasksList.innerHTML = tasksHtml;
   } 
-};
+}
 
 
